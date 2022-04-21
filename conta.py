ca3d07d7793d7be05b1ab7cb565330490739dfa9
Jhony de Oliveira Rodrigues <jhony.o_rodrigues@hotmail.com>
2022-04-20 22:02:17 -0300

    implementando métodos

--- conta.py
+++ conta.py
@@ -1,5 +1,4 @@
 
-
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
@@ -7,3 +6,22 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print("Saldo de R$ {}, do titular: {}".format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saque(self, valor):
+        if self.saldo > valor:
+            self.saldo -= valor
+        else:
+            print("Saldo insuficiente, saldo atual: {}".format(self.saldo))
+
+    def transferencia(self, conta, valor):
+        if self.saldo > valor:
+            self.saldo -= valor
+            conta.saldo += valor
+        else:
+            print("Saldo insuficiente, saldo atual: {}".format(self.saldo))
